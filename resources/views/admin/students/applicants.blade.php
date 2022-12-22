1cf2546fdd76e547f7e9c531b9306de06ff0ef33
Franklin <85135768+K-NURF@users.noreply.github.com>
2022-12-22 14:22:58 +0300

    applicant view change

--- resources/views/admin/students/applicants.blade.php
+++ resources/views/admin/students/applicants.blade.php
@@ -14,7 +14,19 @@
                 <th> </th>
             </tr>
             @foreach ($applicants as $applicant)
-                <x-applicant-card :applicant="$applicant" />
+            <tr class="p-2">
+                <td class="p-2">{{ $applicant->id }}</td>
+                <td class="p-2">{{ $applicant->name }}</td>
+                <td class="p-2">{{ $applicant->email }}</td>
+                <td class="p-2">{{ $applicant->phone_number }}</td>
+                <td class="p-2">{{ $applicant->country }}</td>
+                <td class="p-2">
+                    <x-primary-button> Accept </x-primary-button>
+                </td>
+                <td class="p-2">
+                    <x-danger-button> Decline </x-danger-button>
+                </td>
+            </tr>
             @endforeach
         </table>
 </x-card>
