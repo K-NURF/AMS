e35cfd4fec4f2ee99f037f65284c66a08465dd1d
mumbiann <107537497+mumbiann@users.noreply.github.com>
2023-01-02 19:22:34 +0300

    css

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -21,7 +21,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <img class="h-20" src="images/logo.png" alt="">
+                    <img class="h-20" src="images/schoologo.png" alt="">
                 </a>
             </div>
 
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('admin') }}">
-                        <img class="h-16" src="/images/logo.png" alt="">
+                        <img class="h-16" src="/images/schoologo.png" alt="">
                     </a>
                 </div>
 
